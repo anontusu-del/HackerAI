ba3abf7e61c7e21930dcec4fa8ff3d16c5fcd97c
anontusu-del <anontusu@gmail.com>
2026-08-20 00:24:00 +0600

    Delete gmb-pro-dashboard directory

--- gmb-pro-dashboard/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title GMB Pro Dashboard
-cd /d "%~dp0"
-echo Starting GMB Pro Dashboard...
-echo Open http://localhost:3000 in your browser
-node server.js
-pause
-
--- gmb-pro-dashboard/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")"
-echo "Starting GMB Pro Dashboard..."
-echo "Open http://localhost:3000 in your browser"
-node server.js
-
